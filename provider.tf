a6ec980dda2c5a2025c72087e76b8e2643ad113b
zohirul97 <zohirul97@gmail.com>
2024-06-11 16:42:00 -0400

    Adding Terraform distroy pipeline

--- provider.tf
+++ provider.tf
@@ -1,3 +1,11 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "atlastech-state-bucket"
+    key            = "terraform.tfstate"
+    region         = "eu-east-1"
+  }
+}
